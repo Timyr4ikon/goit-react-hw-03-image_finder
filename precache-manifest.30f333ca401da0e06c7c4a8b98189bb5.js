19659bb1f732df71bb48d643a51f2ce0b34a9651
Timyr4ikon <54175850+Timyr4ikon@users.noreply.github.com>
2020-06-21 03:36:46 +0300

    Updates

---
+++ precache-manifest.30f333ca401da0e06c7c4a8b98189bb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2909a9c6daffba2776bab885bb35544f",
+    "url": "/goit-react-hw-03-image_finder/index.html"
+  },
+  {
+    "revision": "4b75defe3879d1520fcb",
+    "url": "/goit-react-hw-03-image_finder/static/css/main.6454d8e5.chunk.css"
+  },
+  {
+    "revision": "ed7b3fcf0b81c9f818c6",
+    "url": "/goit-react-hw-03-image_finder/static/js/2.93930555.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image_finder/static/js/2.93930555.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b75defe3879d1520fcb",
+    "url": "/goit-react-hw-03-image_finder/static/js/main.97805f59.chunk.js"
+  },
+  {
+    "revision": "b3adab004a39ac8120ca",
+    "url": "/goit-react-hw-03-image_finder/static/js/runtime-main.8ab7c70f.js"
+  }
+]);
